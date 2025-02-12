7e35d24e204e75c33ca18dfc470a6a89c22a3685
krilen <krister@lenberg.se>
2025-02-12 18:49:59 +0100

    Updated docstrings and remove _

--- v06/veckouppgift/e3_bank/bank.py
+++ v06/veckouppgift/e3_bank/bank.py
@@ -1,4 +1,5 @@
 """
+Exercise Week6 E3:
  * sätta in pengar (deposit)
  * ta up pengar (withdraw)
  * returnera nuvarande saldo (balance)
@@ -7,10 +8,15 @@
 """
 
 class Bank():
+    """
+    A class to handle an account and do different things to the amount that the account holds.
+    """
     
     # Default för att sätta upp class
     def __init__(self, amount: int | float) -> None:
-        
+        """
+        Setup of the amount in the account.
+        """
         if not isinstance(amount, (int, float)):
             self.__amount = 0.00
             
@@ -21,33 +27,47 @@ class Bank():
     # Getter för balance of the amount in the account
     @property
     def balance(self):
+        """
+        Returns the current amount in the account.
+        """
         return round(self.__amount, 2)
     
     
     # Method to deposit an amount
-    def deposit(self, _deposit: int | float) -> float:
-        
-        if isinstance(_deposit, (int, float)):
-            self.__amount += _deposit
+    def deposit(self, deposit: int | float) -> float:
+        """
+        Adds (deposit) an amount to the account.
+        Returns the updated current balance.
+        """
+        if isinstance(deposit, (int, float)):
+            self.__amount += deposit
         
         return self.balance
     
     
     # Method to withdraw an amount
-    def withdraw(self, _withdraw: int | float) -> None | float:
-        if not isinstance(_withdraw, (int, float)):
+    def withdraw(self, withdraw: int | float) -> None | float:
+        """
+        Removes (withdraw) an amount from the account.
+        Returns 'None' if the withdraw is not possible, if possible it returns the updated current balance.
+        """
+        if not isinstance(withdraw, (int, float)):
             return None
         
-        if self.balance - _withdraw < 0:
+        if self.balance - withdraw < 0:
             return None
         
-        self.__amount -= _withdraw
+        self.__amount -= withdraw
                 
         return self.balance
     
     
     # Method for adding a rate
     def add_intrest_rate(self, rate: int | float) -> float:
+        """
+        Calculates an amount from the intrest rate and adds it to your account.
+        Returns 0.00 if no intrest is possible, otherwise it returns the earned intrest amount.
+        """
         if not isinstance(rate, (int, float)):
             return 0.00
         
@@ -62,6 +82,10 @@ class Bank():
         
     # Method to check if a specifi invoice can be paid
     def check_invoice(self, invoice_amount: int | float) -> bool:
+        """
+        Checks to see if an invoice can be payed with the current amount.
+        Returns only 'True' or 'False' if the invoice can be payed.
+        """
         if not isinstance(invoice_amount, (int, float)):
             return False
         
